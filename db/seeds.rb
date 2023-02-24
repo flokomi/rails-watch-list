70865859d7c4d873f2cc4a39eecb956c35b71dcd
Florian Miller <flokomi@web.de>
2023-02-24 11:13:28 +0100

    app fuctionality works

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,23 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   movies = Movie.create([{ name: 'Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Deleting exising movies'
+Movie.destroy_all
+
+puts 'Fetching movies from API and filling DB)'
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+serialized_movies = URI.open(url).read
+movies = JSON.parse(serialized_movies)
+movies['results'].each do |movie|
+  Movie.create(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w200/#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+puts "Created #{Movie.count} movies!"
